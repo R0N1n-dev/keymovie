6ffd628db913cbab643d918c9389d2e6a9510990
Ronin <mdrewj@live.com>
2021-12-28 07:59:50 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const { Keystone } = require("@keystonejs/keystone");
+const { Text, Integer } = require("@keystonejs/fields");
+const { GraphQLApp } = require("@keystonejs/app-graphql");
+const { AdminUIApp } = require("@keystonejs/app-admin-ui");
+const { StaticApp } = require("@keystonejs/app-static");
+
+const { MongooseAdapter: Adapter } = require("@keystonejs/adapter-mongoose");
+const PROJECT_NAME = "Movie Rating";
+
+const keystone = new Keystone({
+  name: PROJECT_NAME,
+  adapter: new Adapter({
+    mongoUri: process.env.MONGOURI || "mongodb://localhost/keystone",
+  }),
+  cookie: {
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 1000 * 60 * 60 * 24 * 30,
+    sameSite: false,
+  },
+  cookieSecret: process.env.COOKIE_SECRET || "v3rg1l",
+});
+
+// Define schema for movie ratings
+keystone.createList("Movie", {
+  fields: {
+    title: {
+      type: Text,
+      isRequired: true,
+      isUnique: true,
+    },
+    rating: {
+      type: Integer,
+      isRequired: true,
+      defaultValue: 10,
+    },
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new StaticApp({ path: "/", src: "public" }),
+    new AdminUIApp({ enableDefaultRoute: true }),
+  ],
+};
